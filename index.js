0b1fc6c345a92c8fd10f4bac1da5441a632519ca
Rajput-Suraj <surajarajput@gmail.com>
2024-01-21 12:59:04 +0530

    Added start and build command

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 const heading = React.createElement('h1', null, 'Hello React!');
-const sunHeading = React.createElement('h2', { id: 'hello' }, 'Hello Everyone');
+const sunHeading = React.createElement('h2', { id: 'hello' }, 'Ignited app with parcel');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
