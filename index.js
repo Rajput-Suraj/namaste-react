d75f6783e85ff9e5c427310caf072524622e9110
Rajput-Suraj <surajarajput@gmail.com>
2024-01-21 12:29:05 +0530

    Created gitignore file and configured app

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 const heading = React.createElement('h1', null, 'Hello React!');
 const sunHeading = React.createElement('h2', { id: 'hello' }, 'Hello Everyone');
 
