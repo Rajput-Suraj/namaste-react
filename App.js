bcb94d31935c68d2e9e43e40bc1a9c1213c1f352
Rajput-Suraj <surajarajput@gmail.com>
2024-01-22 21:53:07 +0530

    Written code in JSX

--- App.js
+++ App.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const heading = React.createElement('h1', null, 'Hello React!');
-const sunHeading = React.createElement('h2', { id: 'hello' }, 'Ignited app with parcel');
+const jsxHeading = <h1>Namaste React using JSX 🚀</h1>;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render([heading, sunHeading]);
+root.render(jsxHeading);
