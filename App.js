aab79998e8d9e13131f5bf86d1fa7e5ea517a5cd
Rajput-Suraj <surajarajput@gmail.com>
2024-01-23 21:42:18 +0530

    Build app layout with header component

--- App.js
+++ App.js
@@ -1,19 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const Title = () => {
-  return <h1>Namaste React 🚀</h1>;
-};
-
-const Container = () => {
+function Header() {
   return (
-    <div id="container">
-      <Title />
-      <h1>This is a functional component</h1>
+    <div className="header flex">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://assets.materialup.com/uploads/81b4aea0-7b9f-47b6-adfc-2524583da17e/preview.png"
+          alt="Logo"
+        />
+      </div>
+      <div className="nav-items">
+        <ul className="flex">
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
     </div>
   );
-};
+}
+
+function AppLayout() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<Container />);
+root.render(<AppLayout />);
