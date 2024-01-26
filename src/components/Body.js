031a25a2c1368d851c7c6564d423da1f762bd69d
Rajput-Suraj <surajarajput@gmail.com>
2024-01-26 14:09:27 +0530

    Implemented basic search functionality to work

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,15 +1,35 @@
-import RestaurantCard from './RestaurantCard';
+import { useState } from 'react';
 
+import RestaurantCard from './RestaurantCard';
 import { restaurantsList } from '../utils/mockData';
 
 function Body() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [resList, setResList] = useState(restaurantsList);
+
+  const handleSearch = () => {
+    if (searchTerm) {
+      const filteredData = resList?.filter((res) => res.info.name.includes(searchTerm));
+      setResList(filteredData);
+    }
+  };
+
   return (
     <div className="container">
       <div className="search">
-        <input type="text" className="search-input" placeholder="Search for restaurant and food" />
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="search-input"
+          placeholder="Search for restaurant and food"
+        />
+        <button className="search-btn" onClick={handleSearch}>
+          Search
+        </button>
       </div>
       <div className="grid-container gap-30 mtb-20">
-        {restaurantsList.map((restaurant) => (
+        {resList.map((restaurant) => (
           <RestaurantCard key={restaurant?.info?.id} restaurant={restaurant} />
         ))}
       </div>
