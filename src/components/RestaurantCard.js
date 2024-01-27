ceb8b4818445d4b48424316ba973ba605fbbde33
Rajput-Suraj <surajarajput@gmail.com>
2024-01-27 12:04:31 +0530

    Updated delivery key

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -11,7 +11,7 @@ function RestaurantCard({ restaurant }) {
         <h3>{name}</h3>
         <h4>{cuisines.join(',')}</h4>
         <h4>{avgRating} Rating</h4>
-        <h4>{sla?.deliveryTime} mins</h4>
+        <h4>{sla?.slaString}</h4>
         <h4>{costForTwo}</h4>
       </div>
     </div>
