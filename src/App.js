e44fb8efbcb321cf8ec6ec92b3c0b1f102ac5a03
Rajput-Suraj <surajarajput@gmail.com>
2024-01-27 13:50:25 +0530

    Implemented routing and added different pages

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Outlet, createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+import About from './pages/About';
+import Error from './pages/Error';
 import Body from './components/Body';
+import Contact from './pages/Contact';
 import Header from './components/Header';
 
 function AppLayout() {
   return (
-    <div>
+    <>
       <Header />
-      <Body />
-    </div>
+      <Outlet />
+    </>
   );
 }
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <AppLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Body />,
+      },
+      {
+        path: '/about',
+        element: <About />,
+      },
+      {
+        path: '/contact',
+        element: <Contact />,
+      },
+    ],
+    errorElement: <Error />,
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<AppLayout />);
+root.render(<RouterProvider router={router} />);
