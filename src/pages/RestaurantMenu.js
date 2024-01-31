4986179e8606758597c9ee8e963e60f612756602
Rajput-Suraj <surajarajput@gmail.com>
2024-01-31 16:02:52 +0530

    Created a async function to get restaurant menu using API

--- src/pages/RestaurantMenu.js
+++ src/pages/RestaurantMenu.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 function RestaurantMenu() {
   const { id } = useParams();
+
+  useEffect(() => {
+    async function getRestaurantMenu() {
+      const response = await fetch(`${process.env.REACT_APP_MENU_URL}${id}`);
+
+      const data = await response.json();
+      console.log(data?.data?.cards);
+    }
+
+    getRestaurantMenu();
+  }, []);
+
   return (
     <div className="padding-container">
       <h1>Menu</h1>
