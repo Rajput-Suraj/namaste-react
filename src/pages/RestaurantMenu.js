7ff0a7d2172bc9cad6da0133d86676f075274a13
Rajput-Suraj <surajarajput@gmail.com>
2024-01-31 15:53:55 +0530

    Used useParam hook to get the restaurant ID

--- src/pages/RestaurantMenu.js
+++ src/pages/RestaurantMenu.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 function RestaurantMenu() {
+  const { id } = useParams();
   return (
     <div className="padding-container">
       <h1>Menu</h1>
