431e345e59054a148db6ebd15bba3f3a74da8b54
Rajput-Suraj <surajarajput@gmail.com>
2024-02-01 14:37:08 +0530

    Displayed restaurant menu list

--- src/pages/RestaurantMenu.js
+++ src/pages/RestaurantMenu.js
@@ -1,23 +1,39 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 function RestaurantMenu() {
   const { id } = useParams();
+  const [restaurantDetails, setRestaurantDetails] = useState([]);
 
   useEffect(() => {
     async function getRestaurantMenu() {
       const response = await fetch(`${process.env.REACT_APP_MENU_URL}${id}`);
 
       const data = await response.json();
-      console.log(data?.data?.cards);
+      setRestaurantDetails(data?.data?.cards);
     }
 
     getRestaurantMenu();
   }, []);
 
+  const menuList =
+    restaurantDetails[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card?.itemCards;
+
   return (
     <div className="padding-container">
-      <h1>Menu</h1>
+      <div className="details">
+        <h1>{restaurantDetails[0]?.card?.card?.info?.name}</h1>
+        <h2>Menu</h2>
+        <ul>
+          {menuList?.map((menu) => {
+            return (
+              <li key={menu?.card?.info?.id}>
+                {menu?.card?.info?.name} - Rs.{menu?.card?.info?.price / 100}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
