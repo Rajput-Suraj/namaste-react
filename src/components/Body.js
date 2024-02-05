bdd6407009747dc4cf2c7849ac134073d67c3a11
Rajput-Suraj <surajarajput@gmail.com>
2024-02-05 15:18:21 +0530

    Designed search input and button

--- src/components/Body.js
+++ src/components/Body.js
@@ -21,16 +21,19 @@ function Body() {
   return resList.length === 0 ? (
     <Shimmer />
   ) : (
-    <div className="container">
-      <div className="search">
+    <div className="mx-[50px]">
+      <div className="flex my-4 items-center justify-center">
         <input
           type="text"
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
-          className="search-input"
+          className="mx-3 border border-solid border-gray-200 rounded-sm p-1 w-[350px] h-9 text-base"
           placeholder="Search for restaurant and food"
         />
-        <button className="search-btn" onClick={handleSearch}>
+        <button
+          className="bg-orange-500 p-1 rounded-sm text-slate-100 h-9 w-14 text-base"
+          onClick={handleSearch}
+        >
           Search
         </button>
       </div>
