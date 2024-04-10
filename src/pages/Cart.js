00e2a2b18b74c033eb7f99e8bad9fe014a8a4330
Rajput-Suraj <surajarajput@gmail.com>
2024-04-10 16:50:13 +0530

    Started design of cart page

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,9 +1,34 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 
+import VegIcon from '../assets/images/veg.png';
+import NonVegIcon from '../assets/images/non-veg.png';
+
 function Cart() {
   const { cart } = useSelector((state) => state.cart);
-  return <div className="">Cart</div>;
+  console.log(cart);
+  return (
+    <div className="flex justify-end">
+      <div className="m-3 p-3 bg-slate-200 w-2/4">
+        {cart?.map((item) => (
+          <div key={item?.id} className="text-sm flex justify-between items-center pb-2">
+            <div className="flex items-center justify-start gap-2">
+              <img src={item?.isVeg ? VegIcon : NonVegIcon} className="h-5" alt="Icon" />
+              <span className="text-ellipsis whitespace-wrap overflow-hidden">
+                {item?.name?.length > 30 ? `${item?.name.slice(0, 14)}...` : item?.name}
+              </span>
+              <div>
+                x <span className="font-medium">{item?.quantity}</span>
+              </div>
+            </div>
+            <span>
+              ₹ {item?.price ? item?.price / 100 : (item?.defaultPrice / 100) * item?.quantity}
+            </span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Cart;
