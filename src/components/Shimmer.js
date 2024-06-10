51214ce1a0eba90931468d0e8a5ea67495992010
Suraj Rajput <49227244+Rajput-Suraj@users.noreply.github.com>
2024-06-10 17:42:20 +0530

    Update Shimmer.js.

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -2,7 +2,7 @@ function Shimmer() {
   return (
     <div className="border border-slate-300 rounded-md p-4 max-w-sm w-64 mx-auto">
       <div className="animate-pulse flex space-x-4"></div>
-      <div className="rounded-md bg-slate-300 h-24 w-full mb-2"></div>
+      <div className="rounded-md bg-slate-400 h-24 w-full mb-2"></div>
       <div className="flex-1 space-y-6 py-1">
         <div className="h-5 bg-slate-300 rounded"></div>
         <div className="space-y-3">
