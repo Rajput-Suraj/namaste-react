279298338abcf1a31f4fb9e125d7e11a700819a6
Suraj Rajput <49227244+Rajput-Suraj@users.noreply.github.com>
2024-06-11 19:16:17 +0530

    Update Shimmer.js

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -7,7 +7,7 @@ function Shimmer() {
         <div className="h-5 bg-slate-300 rounded"></div>
         <div className="space-y-3">
           <div className="grid grid-cols-3 gap-4">
-            <div className="h-2 bg-slate-300 rounded col-span-2"></div>
+            <div className="h-2 bg-slate-400 rounded col-span-2"></div>
             <div className="h-2 bg-slate-300 rounded col-span-1"></div>
           </div>
           <div className="h-2 bg-slate-300 rounded"></div>
